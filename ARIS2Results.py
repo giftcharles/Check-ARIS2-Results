93d37b9fa0691420e1086021f749531c02118daa
giftcharles <mcrider45g@gmail.com>
2019-07-31 14:54:33 +0300

    updated Docs

--- ARIS2Results.py
+++ ARIS2Results.py
@@ -1,31 +1,30 @@
 #!/usr/bin/env python
 
 """
-Check ARIS2 Results
-Periodically check for results update published on the 
-ARIS website for any given account/student
+# Check ARIS2 Results
+Periodically check for results update published on the ARIS website for any given account/student
 
-How to use
-1 Create credentials.py file.
 
-2 Add three variables, __username__, __password__ and __waittime__.
-The __username__ variable takes your ARIS2 ID as a value,
- the __password__ variable obviously takes your password and the 
-__waittime__ variable takes an integer that defines the amount of time
- to wait between each check, specified in seconds.
+# How to use
 
-3 Use 0 as a value for __waittime__ to make the script wait at a random 
-interval between 12 and 24 hours until the next check.
+1. Create `credentials.py` file.
+2. Add three variables, `__username__`, `__password__`, `__headless__` and `__waittime__`.
+3. The `__username__` variable takes your ARIS2 ID as a value, the `__password__` variable 
+obviously takes your password, the `__waittime__` variable takes an integer that defines 
+the amount of time to wait between each check, specified in seconds and the `__headless__` 
+variable takes a `0` or a `1`, when set to `0` it will show the automated browser while 
+checking for results and it will not show it when set to `1`, if you do not set it the default is `1`.
+4. Use `0` as a value for `__waittime__` to make the script wait at a random interval
+ between 12 and 24 hours until the next check.
+5. Run `ARIS2Results.py` to start the checks.
 
-4 Run ARIS2Results.py to start the checks.
+When the script finds your results they will be automatically downloaded into `full_results.html`.
 
-When the script finds your results they will be automatically
- downloaded into full_results.html.
+# Requirements
 
-Requirements
-1 Python 3.x
-2 Chromedriver
-3 Chrome Browser
+1. Python 3.x
+2. Chromedriver
+3. Chrome Browser
 """
 
 __author__ = "Gift C. Nakembetwa"
